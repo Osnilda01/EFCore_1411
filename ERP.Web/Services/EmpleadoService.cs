f52cf2dbbebea9d565e54e239093b6afc490a5f0
Osnilda01 <2022-1411@uteco.edu.do>
2025-03-11 20:28:14 -0400

    Creacion del servicio EmpleadoService (incompleto)

---
+++ ERP.Web/Services/EmpleadoService.cs
@@ -0,0 +1,43 @@
+﻿using ERP.Web.Data;
+using ERP.Web.Domain.Dto;
+using ERP.Web.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace ERP.Web.Services;
+
+public interface IEmpleadoService
+{
+
+}
+public class EmpleadoService
+{
+    private readonly AppDbContext _context;
+    public EmpleadoService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<EmpleadoDto>> Consultar(string filtro)
+    {
+        var empleados = await
+            _context.Empleados
+            .Include(e => e.DatosPersonales)
+            .Where(e => e.DatosPersonales.Nombre.Contains(filtro))
+            .Select(
+                e =>
+                new EmpleadoDto()
+                {
+                    Id = e.Id,
+                    PersonaId = e.PersonaId,
+                    Sueldo = e.Sueldo,
+                    DatosPersonales = new PersonaDto()
+                    {
+                        Id = e.DatosPersonales.Id,
+                        Nombre = e.DatosPersonales.Nombre,
+                        FechaDeNacimiento = e.DatosPersonales.FechaDeNacimiento
+                    }
+                }
+            )
+            .ToListAsync();
+        return empleados;
+    }
+}
