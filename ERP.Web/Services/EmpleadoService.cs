5827dbc3b40bb86a34029bf61d6b71239ac9805a
Osnilda01 <2022-1411@uteco.edu.do>
2025-03-20 20:26:02 -0400

    Se agregaron las funciones en la interfaz EmpleadoService.

--- ERP.Web/Services/EmpleadoService.cs
+++ ERP.Web/Services/EmpleadoService.cs
@@ -6,7 +6,10 @@ namespace ERP.Web.Services;
 
 public interface IEmpleadoService
 {
-
+	Task<List<EmpleadoDto>> Consultar(string filtro);
+	Task<bool> Crear(EmpleadoDto request);
+	Task<bool> Eliminar(int Id);
+	Task<bool> Modificar(EmpleadoDto request);
 }
 public class EmpleadoService
 {
@@ -33,12 +36,46 @@ public class EmpleadoService
                     DatosPersonales = new PersonaDto()
                     {
                         Id = e.DatosPersonales.Id,
-                        Nombre = e.DatosPersonales.Nombre,
-                        FechaDeNacimiento = e.DatosPersonales.FechaDeNacimiento
+                        Nombre = e.DatosPersonales.Nombre
                     }
                 }
             )
             .ToListAsync();
         return empleados;
     }
+	public async Task<bool> Crear(EmpleadoDto request)
+	{
+		var empleado = Empleado.Create(
+			request.DatosPersonales.Nombre,
+			request.Sueldo,
+			request.Area
+		);
+		_context.Empleados.Add(empleado);
+		;
+		return (await _context.SaveChangesAsync()) > 0;
+	}
+	public async Task<bool> Modificar(EmpleadoDto request)
+	{
+		//1. Busco el cliente
+		var empleado = await _context.Empleados
+			.Include(c => c.DatosPersonales)
+			.FirstOrDefaultAsync(c => c.Id == request.Id);
+		//2. Modifico el cliente
+		empleado!.DatosPersonales.Nombre = request.DatosPersonales.Nombre;
+		empleado!.Sueldo = request.Sueldo;
+		empleado!.Area = request.Area;
+		//Guardo los cambios
+		return (await _context.SaveChangesAsync()) > 0;
+	}
+	public async Task<bool> Eliminar(int Id)
+	{
+		var empleado = await _context.Empleados
+			.FirstOrDefaultAsync(c => c.Id == Id);
+
+		_context.Empleados.Remove(empleado!);
+
+		return (await _context.SaveChangesAsync()) > 0;
+	}
 }
+
+
